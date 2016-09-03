9aeecee7fd19a672d670d21110b09d5bf610fa07
pepperpapa <zhongxin2506@outlook.com>
2016-09-04 00:03:36 +0800

    change application from def to class

--- cgi/app.py
+++ cgi/app.py
@@ -1,9 +1,27 @@
 # python3.5
 # -*- coding: utf-8 -*-
 
-def application(environ, start_response):
-    # 使用uwsgi server，必须使用application作为方法名
-    status = "200 OK"
-    response_headers = [('Content-type', 'text/html')]
-    start_response(status, response_headers)
-    return ['Hello world!\n'.encode('utf-8')]
+# 使用uwsgi server，必须使用application作为方法名或类名
+class application:
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start_response = start_response
+
+    def __iter__(self):
+        path = self.environ['PATH_INFO']
+        if path == "/hello":
+            return self.GET_hello()
+        else:
+            return self.notfound()
+
+    def GET_hello(self):
+        status = "200 OK"
+        response_headers = [('Content-type', 'text/html')]
+        self.start_response(status, response_headers)
+        yield "Hello world!\n".encode("utf-8")
+
+    def notfound(self):
+        status = "404 Not Found"
+        response_headers = [('Content-type', 'text/html')]
+        self.start_response(status, response_headers)
+        yield "Not Found\n".encode("utf-8")
