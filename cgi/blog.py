f19a873c40d3b84f3bbb4cc99cd8054a5239309f
pepperpapa <zhongxin2506@outlook.com>
2016-09-17 17:24:46 +0800

    newpost: subject, content, created, last_modifed

--- cgi/blog.py
+++ cgi/blog.py
@@ -1,10 +1,13 @@
 # python3.5
 # -*- coding: utf-8 -*-
+import re
+import time
+
 if __name__ == '__main__':
     import db
 else:
     from cgi import db
-    
+
 class BlogFront:
     def get(self, app, *args):
         # app为application对象
@@ -24,10 +27,21 @@ class NewPost:
         return content.encode("utf-8")
 
     def post(self, app, *args):
-        new_blog = app.getBody()
-        print(new_blog)
+        new_post = app.getBody()
+        new_post_dict = self.newpost_dict(new_post)
         app.header('Content-type', 'text/html')
-        return new_blog.encode("utf-8")
+        return "success".encode("utf-8")
+
+    def newpost_dict(self, new_post):
+        rule = r'.+=(.+)&.+=(.+)'
+        match = re.match(rule, new_post)
+        new_post = {}
+        new_post["subject"] = match.groups()[0]
+        new_post["conntent"] = match.groups()[1]
+        day = time.strftime("%b %m, %Y", time.localtime())
+        new_post["created"] = day
+        new_post["last_modified"] = day
+        return new_post
 
 blogFront = BlogFront()
 newPost = NewPost()
