0a775ac758ec5f38db6230cb8edf4cfbb967c391
pepperpapa <zhongxin2506@outlook.com>
2016-10-02 15:53:13 +0800

    limit load 15 posts

--- cgi/db.py
+++ cgi/db.py
@@ -92,7 +92,7 @@ class Blog:
     def getAllPosts(self):
         # 如果posts不存在则首先创建表posts
         self.conn, self.cursor = self.createBlogTable()
-        self.cursor.execute("SELECT * FROM posts ORDER BY ID DESC")
+        self.cursor.execute("SELECT * FROM posts ORDER BY ID DESC LIMIT 15")
         query_posts = self.cursor.fetchall()
         self.conn.close()
         return query_posts
