0b0c70d62d4d70b3cd55f5039f0db7dbb7d02120
pepperpapa <zhongxin2506@outlook.com>
2016-10-04 00:29:19 +0800

    update styles

--- cgi/sign.py
+++ cgi/sign.py
@@ -135,7 +135,7 @@ class Logout:
     def get(self, app, *args):
         # after logout, set login.user equal None
         login.user = None
-        print(login.user)
+
         app.header("Set-Cookie", "user_id=;")
         return app.redirect('/myblog/signup')
 
